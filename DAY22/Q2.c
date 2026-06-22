7879bcdbe26791999cc8d0a2a94bf7b61d020d91
Anmol Khari <anmolkhari05@gmail.com>
2026-06-22 22:26:08 +0530

    DAY22 uploaded

---
+++ DAY22/Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[200];
+    int i, words = 0;
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++) {
+        if ((i == 0 && str[i] != ' ' && str[i] != '\n') ||
+            (str[i] != ' ' && str[i - 1] == ' '))
+            words++;
+    }
+    printf("Total words = %d", words);
+    return 0;
+}
